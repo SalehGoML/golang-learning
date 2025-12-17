479d47400cc85168006839452d766abaab23cd4a
SalehAskarzadeh <msaskarzadeh@gmail.com>
2025-12-17 09:35:44 +0330

    review -struct

--- main.go
+++ main.go
@@ -1,69 +1,101 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	var users = map[string]int{}
+//
+//	users["saleh"] = 2
+//
+//	fmt.Println(users)
+//
+//	for key, value := range users {
+//		fmt.Println(key, value)
+//	}
+//
+//	var u = map[int]int{}
+//
+//	fmt.Println(u)
+//	fmt.Println(u[3])
+//
+//	// ok paradigm
+//	value, ok := u[3]
+//	if !ok {
+//		fmt.Println("there is no value of key 3")
+//	} else {
+//		fmt.Println("value:", value)
+//	}
+//
+//	type user struct {
+//		ID    int
+//		Name  string
+//		Email string
+//	}
+//
+//	var userList = make(map[uint]user)
+//
+//	userList[1] = user{
+//		ID:    1,
+//		Name:  "saleh",
+//		Email: "sa@Sa",
+//	}
+//	fmt.Println("userList[1]", userList[1])
+//
+//	var userL = make(map[user]int)
+//
+//	userL[user{
+//		ID:    1,
+//		Name:  "saleh",
+//		Email: "sa@Sa",
+//	}] = 100
+//
+//	v1, ok := userL[user{
+//		ID: 1,
+//	}]
+//	fmt.Println("test userL 1", v1, ok)
+//
+//	v2, ok := userL[user{
+//		ID:   1,
+//		Name: "saleh",
+//	}]
+//	fmt.Println("test userL 2", v2, ok)
+//
+//	v3, ok := userL[user{
+//		ID:    1,
+//		Name:  "saleh",
+//		Email: "sa@Sa",
+//	}]
+//	fmt.Println("test userL 3", v3, ok)
+//}
+
 package main
 
 import "fmt"
 
 func main() {
-	var users = map[string]int{}
 
-	users["saleh"] = 2
+	var u1 User
+	fmt.Println("u1", u1)
+	u1.Email = "saleh@.com"
+	fmt.Println(u1.Email)
 
-	fmt.Println(users)
+	u1.pr()
 
-	for key, value := range users {
-		fmt.Println(key, value)
-	}
+	pr("saleh")
 
-	var u = map[int]int{}
+}
 
-	fmt.Println(u)
-	fmt.Println(u[3])
+type User struct {
+	ID       uint
+	Name     string
+	Email    string
+	IsActive bool
+}
 
-	// ok paradigm
-	value, ok := u[3]
-	if !ok {
-		fmt.Println("there is no value of key 3")
-	} else {
-		fmt.Println("value:", value)
-	}
+func (u User) pr(name string) {
+	fmt.Println(u.Name)
+}
 
-	type user struct {
-		ID    int
-		Name  string
-		Email string
-	}
-
-	var userList = make(map[uint]user)
-
-	userList[1] = user{
-		ID:    1,
-		Name:  "saleh",
-		Email: "sa@Sa",
-	}
-	fmt.Println("userList[1]", userList[1])
-
-	var userL = make(map[user]int)
-
-	userL[user{
-		ID:    1,
-		Name:  "saleh",
-		Email: "sa@Sa",
-	}] = 100
-
-	v1, ok := userL[user{
-		ID: 1,
-	}]
-	fmt.Println("test userL 1", v1, ok)
-
-	v2, ok := userL[user{
-		ID:   1,
-		Name: "saleh",
-	}]
-	fmt.Println("test userL 2", v2, ok)
-
-	v3, ok := userL[user{
-		ID:    1,
-		Name:  "saleh",
-		Email: "sa@Sa",
-	}]
-	fmt.Println("test userL 3", v3, ok)
+func pr(name string) {
+	fmt.Println(name)
 }
